f99bb545cd33b5102ffafcae90f83884ac4681a8
1etdown <artem.ashurkin12@gmail.com>
2024-12-17 16:34:18 +0300

    Add new song

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 SONGS_DB = [
     {"id": 1, "title": "Like a Rolling Stone", "artist": "Bob Dylan"},
     {"id": 2, "title": "Imagine", "artist": "John Lennon"},
-    {"id": 3, "title": "Smells Like Teen Spirit", "artist": "Nirvana"}
+    {"id": 3, "title": "Smells Like Teen Spirit", "artist": "Nirvana"},
+    {"id": 4, "title": "Comfortably Numb", "artist": "Pink Floyd"}
 ]
 
 @app.route('/')
